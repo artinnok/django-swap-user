e7127064f6907cfebe08e5658f0b17c9cb4fc23b
Artem Innokentiev <artinnok@mail.ru>
2020-11-12 15:35:32 +0900

    Update verbose_name of apps

--- swap_user/email/apps.py
+++ swap_user/email/apps.py
@@ -2,6 +2,10 @@ from django.apps import AppConfig
 
 
 class EmailUserConfig(AppConfig):
+    # `name` for INSTALLED_APPS
     name = "swap_user.email"
+    # `label` is like an alias for application,
+    # used in model relations for example
     label = "swap_user_email"
-    verbose_name = "EmailUser"
+    # `verbose name` for Admin panel display
+    verbose_name = "Swap User"
--- swap_user/named_email/apps.py
+++ swap_user/named_email/apps.py
@@ -2,6 +2,10 @@ from django.apps import AppConfig
 
 
 class NamedEmailUserConfig(AppConfig):
+    # `name` for INSTALLED_APPS
     name = "swap_user.named_email"
+    # `label` is like an alias for application,
+    # used in model relations for example
     label = "swap_user_named_email"
-    verbose_name = "NamedEmailUser"
+    # `verbose name` for Admin panel display
+    verbose_name = "Swap User"
