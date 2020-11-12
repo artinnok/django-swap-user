bea80363fd7602ac66b89a3ebe68c7c4705bdbd5
Artem Innokentiev <artinnok@mail.ru>
2020-11-12 15:27:40 +0900

    Added ls

--- swap_user/named_email/admin.py
+++ swap_user/named_email/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
+from swap_user.named_email.forms import NamedUserEmailForm
 from swap_user.named_email.models import NamedEmailUser
 
-admin.site.register(NamedEmailUser)
+
+class NamedEmailUserAdmin(admin.ModelAdmin):
+    form = NamedUserEmailForm
+
+
+admin.site.register(NamedEmailUser, NamedEmailUserAdmin)
