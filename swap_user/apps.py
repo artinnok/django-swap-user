e8c068a93a5b50cd5084b66c9e55bcd49ae1e9c5
Artem Innokentiev <artinnok@mail.ru>
2020-11-13 13:23:08 +0900

    Added tests for form

---
+++ swap_user/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DjangoSwapUser(AppConfig):
+    name = "swap_user"
+    verbose_name = "Django Swap User"
--- swap_user/tests/test_forms.py
+++ swap_user/tests/test_forms.py
@@ -3,9 +3,56 @@ from unittest.mock import MagicMock
 from swap_user.to_named_email.forms import NamedUserEmailForm
 
 
-def test_empty_password():
+def test_save_field():
     instance = MagicMock()
-    data = {"first_name": "Hello", "last_name": "World"}
+    data = {
+        "email": "hello@world.com",
+        "first_name": "Hello",
+        "last_name": "World",
+    }
     form = NamedUserEmailForm(data=data, instance=instance)
 
-    assert form.is_valid() == False
+    assert form.is_valid() is True
+
+
+def test_one_password():
+    instance = MagicMock()
+    data = {
+        "email": "hello@world.com",
+        "password_1": "Hello",
+    }
+    form = NamedUserEmailForm(data=data, instance=instance)
+
+    assert form.is_valid() is False
+    assert (
+        form.errors.as_json()
+        == '{"__all__": [{"message": "Provide both of passwords", "code": "provide_both_passwords"}]}'
+    )
+
+
+def test_not_matching_passwords():
+    instance = MagicMock()
+    data = {
+        "email": "hello@world.com",
+        "password_1": "Hello",
+        "password_2": "World",
+    }
+    form = NamedUserEmailForm(data=data, instance=instance)
+
+    assert form.is_valid() is False
+    assert (
+        form.errors.as_json()
+        == '{"__all__": [{"message": "Passwords should be same", "code": "password_should_be_same"}]}'
+    )
+
+
+def test_matching_passwords():
+    instance = MagicMock()
+    data = {
+        "email": "hello@world.com",
+        "password_1": "Hello",
+        "password_2": "Hello",
+    }
+    form = NamedUserEmailForm(data=data, instance=instance)
+
+    assert form.is_valid() is True
