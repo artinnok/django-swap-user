c30c3a27ff2660d9fd1cc2a439a44d9335bede3f
Artem Innokentiev <artinnok@mail.ru>
2020-11-13 14:03:44 +0900

    Added explicit label

--- swap_user/to_email/apps.py
+++ swap_user/to_email/apps.py
@@ -4,5 +4,8 @@ from django.apps import AppConfig
 class EmailUserConfig(AppConfig):
     # `name` for INSTALLED_APPS
     name = "swap_user.to_email"
+    # `label` used for relations reference in format `app_label.Model`
+    # also it used for `AUTH_USER_MODEL` settings and fixtures definition
+    label = "to_email"
     # `verbose name` for Admin panel display
     verbose_name = "Django Swap User"
