22698517503cc3f3e089a99ca0f9a5b1bf15410b
Artem Innokentiev <artinnok@mail.ru>
2022-02-09 15:24:24 +0900

    Added GetOTPService, StdOutOTPSender, AbstractOTPSender

--- swap_user/to_email_otp/services.py
+++ swap_user/to_email_otp/services.py
@@ -1,3 +1,36 @@
-class OTPSender:
-    def send(self):
-        print("SEND EMAIL")
+from typing import Optional
+
+from django.contrib.auth import get_user_model
+
+from swap_user.helpers import generate_otp, get_otp_cache_key, set_otp_to_cache
+from swap_user.settings import swap_user_settings
+
+
+UserModel = get_user_model()
+
+
+class GetOTPService:
+    def generate_otp_and_send(self, username: str):
+        user_id = self.get_user_id(username)
+        if not user_id:
+            return None
+
+        sender_class = swap_user_settings.OTP_SENDER_CLASS
+
+        otp = generate_otp()
+        cache_key = get_otp_cache_key(user_id)
+        set_otp_to_cache(cache_key, otp)
+
+        sender = sender_class()
+        sender.send(username, otp)
+
+    def get_user_id(self, username: str) -> Optional[str]:
+        try:
+            username_field = UserModel.USERNAME_FIELD
+            query_data = {username_field: username}
+            user = UserModel.objects.get(**query_data)
+        except UserModel.DoesNotExist:
+            return None
+
+        user_id = user.id
+        return user_id
